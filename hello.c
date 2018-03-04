6e4a5dbc636c96a26199a81087e0ff6f086fb561
kushagra <kushraj1532@gmail.com>
2018-03-05 00:20:09 +0530

    19

--- hello.c
+++ hello.c
@@ -1,6 +1,6 @@
-hello.c
 #include<stdio.h>
 void microkernel_sendmsg(char *);
+void microkernel_getmsg(char *);
 void main(){
 printf("Helloworld!\n");
 printf("This must be a monolithic design\n");
@@ -8,4 +8,7 @@ microkernel_sendmsg("is more portable");
 }
 void microkernel_sendmsg(char *a){
 printf("microkernel: %s\n", a);
+}
+void microkernel_getmsg(char *b){
+//TODO: getmsg feature
 }
